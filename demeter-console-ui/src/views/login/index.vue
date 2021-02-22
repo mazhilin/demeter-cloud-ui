79233ad23533eefb788286ea0023c1458aaa00a8
marklin <marklin1992@outlook.com>
2021-02-22 13:48:25 +0800

    提交代码

--- demeter-console-ui/src/views/login/index.vue
+++ demeter-console-ui/src/views/login/index.vue
@@ -127,7 +127,7 @@ export default {
           }).catch(response => {
             this.$notify.error({
               title: '失败',
-              message: response.data.message
+              message: response.data
             })
             this.loading = false
           })
